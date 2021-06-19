a297096eea9ad127f728eeda22ce21d7c5abb3b9
Hamzanazir0 <Hamzanazir0@gmail.com>
2021-06-20 04:22:53 +0500

    123

---
+++ myreact/src/Components/team.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const team = () => {
+  return (
+    <div>
+      <div id="content" class="site-content center-relative">
+        <div class="member-item-wrapper center-relative">
+          <div class="member-content">
+            <div class="">
+              <div class="info-text">
+                Hello there! I am <b>Hamza</b>. <br />I am{" "}
+                <i>
+                  <b>MERN Stack Developer</b>
+                </i>{" "}
+                and a<br />
+                <i>
+                  <b>Graphics Designer</b>
+                </i>
+                .
+              </div>
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="one_half">
+              <strong>Hamza Nazir,</strong>I am freelancer and have been working
+              on different platforms. In the time i have created many websites
+              and designs.
+              <p>&nbsp;</p>
+              <div class="social">
+                <a href="#" target="_blank">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </div>
+              <div class="social">
+                <a href="#" target="_blank">
+                  <i class="fa fa-instagram"></i>
+                </a>
+              </div>
+              <p>&nbsp;</p>
+              <p>
+                Name: <strong>Hamza Nazir</strong>
+              </p>
+              <p>
+                Age: <strong>21</strong>
+              </p>
+              <p>
+                Main Skill: <strong>JS, HTML, CSS, Photoshop</strong>
+              </p>
+            </div>
+
+            <div class="one_half last">
+              <img src="images/HamzaTeam.jpg" alt="" />
+            </div>
+            <div class="clear"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default team;
