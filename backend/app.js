360a76fef9807adebf6478811096ae8d800838c1
Hamzanazir0 <Hamzanazir0@gmail.com>
2021-06-23 14:43:50 +0500

    Helllo

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
+var teamRouter = require("./routes/team");
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/team", teamRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,22 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-var teamModel = require("../models/teamModel");
-
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  let team = await teamModel.find();
-  console.log(team);
-  return res.send(team);
-});
-
-
-//Insert a record
-router.post("/", async (req, res) => {
-  let team = new teamModel();
-  team.name = req.body.name;
-  await team.save();
-  return res.send(team);
+  res.render("index" , {title:"Home Page"});
 });
 
 module.exports = router;
